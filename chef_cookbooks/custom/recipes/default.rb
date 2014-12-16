26e8e7a531a29b4f580637a4a9d36a92a644a89e
Ryan Alyn Porter <rap@endymion.com>
2014-12-16 18:57:14 -0500

    Ruby support.

--- chef_cookbooks/custom/recipes/default.rb
+++ chef_cookbooks/custom/recipes/default.rb
@@ -8,9 +8,10 @@ include_recipe "nginx-php-fpm"
 %w( openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev
   libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool
   bison subversion pkg-config libmysqlclient-dev ruby-dev imagemagick
-  libcurl3 libcurl3-gnutls libcurl4-openssl-dev ).each do |pkg|
+  libcurl3 libcurl3-gnutls libcurl4-openssl-dev rubygems ruby-bundler ).each do |pkg|
   package pkg do
     action :install
     ignore_failure true
   end
 end
+
